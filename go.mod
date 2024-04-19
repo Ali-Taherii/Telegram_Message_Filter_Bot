38f61c230ad100ef531623c937d0e77106d0880b
Ali Taheri <alitaheri473@gmail.com>
2024-04-19 14:37:10 +0300

    Added /filter command to get the filtering word

---
+++ go.mod
@@ -0,0 +1,3 @@
+module telegram_message_filter/bot
+
+go 1.22.2
--- telegram_bot/structs/tele_bot.go
+++ telegram_bot/structs/tele_bot.go
@@ -8,8 +8,9 @@ import (
 )
 
 type TeleBot struct {
-	API        *tgbotapi.BotAPI
-	StoredWord string
+	API            *tgbotapi.BotAPI
+	StoredWord     string
+	WaitingForWord bool // New state variable to track if bot is waiting for filter word
 }
 
 func NewBot(token string) (*TeleBot, error) {
@@ -33,17 +34,56 @@ func (b *TeleBot) StartListening() {
 		if update.Message != nil {
 			if update.Message.IsCommand() && update.Message.Command() == "start" {
 				b.Start(update)
+			} else if update.Message.IsCommand() && update.Message.Command() == "filter" {
+				b.Filter(update)
+				b.WaitingForWord = true // Set WaitingForWord to true when /filter command received
 			} else {
+				if b.WaitingForWord {
+					b.WordReceiver(update)
+				}
+			} /*else {
 				b.ProcessMessage(update)
-			}
+			}*/
 		}
 	}
 }
 
 func (b *TeleBot) Start(update tgbotapi.Update) {
-	reply := "Welcome! This bot will first ask you for a word, and then for a sentence. It will then check if the sentence contains the word or not."
+	reply := "Welcome! This bot will first ask you for a word, and then for a sentence. It will then check if the sentence contains the word or not.\nUse /filter to define the filer word"
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
 	b.API.Send(msg)
+}
+
+func (b *TeleBot) Filter(update tgbotapi.Update) {
+
+	// Prompt user to enter a word
+	reply := "Write the filter word (one word only)"
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+	b.API.Send(msg)
+}
+
+func (b *TeleBot) WordReceiver(update tgbotapi.Update) (wordReceived bool) {
+	// Reset the forward watcher
+	b.WaitingForWord = false
+
+	// Split the sentence into words
+	words := strings.Fields(update.Message.Text)
+
+	// The user input is more than one word
+	if len(words) > 1 {
+		reply := "Please provide only one word. Try /filter again."
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+		b.API.Send(msg)
+		return false
+	}
+
+	// Store the word to process
+	b.StoredWord = words[0]
+	reply := "Word received. Please send a sentence containing the word in the next message."
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+	b.API.Send(msg)
+
+	return true
 }
 
 func (b *TeleBot) ProcessMessage(update tgbotapi.Update) {
